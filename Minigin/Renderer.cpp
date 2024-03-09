3c5edfc7e8fdc0a184e899978cf9116068c634e8
Owen Bal <Owen.Bal54@gmail.com>
2024-03-09 17:57:40 +0100

    made imgui_plot available in the project

--- Minigin/Renderer.cpp
+++ Minigin/Renderer.cpp
@@ -2,9 +2,9 @@
 #include "Renderer.h"
 #include "SceneManager.h"
 #include "Texture2D.h"
-#include "imgui.h"
 #include "backends/imgui_impl_sdl2.h"
 #include "backends/imgui_impl_opengl2.h"
+//#include "imgui_plot.h"
 
 int GetOpenGLDriverIndex()
 {
